abf58aaf31f320402790469b8bd5c9fc86cf57b0
quintessentialArmory <40044087+quintessentialArmory@users.noreply.github.com>
2018-07-04 15:08:37 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const passport      = require('passport');
+const session       = require('express-session');
 const ObjectID      = require('mongodb').ObjectID;
 
 const app = express();
@@ -12,6 +14,14 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 /*
 passport.serializeUser((user, done) => {
